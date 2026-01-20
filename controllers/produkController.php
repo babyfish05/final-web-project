1e510f2e79dec8d17684a84aa0c48773697ee385
rafildzuardi <rafildzuardi@gmail.com>
2026-01-20 23:01:53 +0700

    produk model

--- controllers/produkController.php
+++ controllers/produkController.php
@@ -1,14 +1,17 @@
 <?php
-class ProdukModel {
-    public function getAllProduk() {
-        global $conn;
-        $query = "SELECT * FROM produk";
-        $result = mysqli_query($conn, $query);
-        $data = [];
-        while ($row = mysqli_fetch_assoc($result)) {
-            $data[] = $row;
-        }
-        return $data;
+require_once './database/koneksi.php';
+require_once './model/ProdukModel.php';
+
+class ProdukController {
+    private $model;
+
+    public function __construct() {
+        $this->model = new ProdukModel();
+    }
+
+    public function index() {
+        $dataProduk = $this->model->getAllProduk();
+        include './view/produkView.php';
     }
 }
 ?>
